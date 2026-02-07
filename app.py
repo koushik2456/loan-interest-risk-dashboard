69126f6acdd01230bfd5faed5e04bbdf894b79da
koushik2456 <152166214+koushik2456@users.noreply.github.com>
2026-02-08 00:41:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pandas as pd
-import subprocess
-import sys
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "joblib"])
 
 import joblib
 import plotly.express as px
